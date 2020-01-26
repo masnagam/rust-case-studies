294849f5243e8a0b6c1956a8b288b64fba7afe23
Masayuki Nagamachi <masnagam@gmail.com>
2020-01-26 14:45:32 +0900

    tokio-process-stdio-hup: add variables to check the task states

--- tokio-process-stdio-hup/src/main.rs
+++ tokio-process-stdio-hup/src/main.rs
@@ -1,4 +1,7 @@
+use std::pin::Pin;
 use std::process::Stdio;
+use std::sync::{Arc, Mutex};
+use std::task::{Context, Poll};
 use std::time::Duration;
 
 use tokio::prelude::*;
@@ -8,11 +11,18 @@ use tokio::process::Command;
 async fn main() {
     let mut child = Command::new("cat")
         .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
+        .stdout(Stdio::null())
         .spawn()
         .unwrap();
 
-    let mut input = child.stdin.take().unwrap();
+    let writing = Arc::new(Mutex::new(false));
+    let shutdown = Arc::new(Mutex::new(false));
+
+    let mut input = Wrapper {
+        inner: child.stdin.take().unwrap(),
+        writing: writing.clone(),
+        shutdown: shutdown.clone(),
+    };
 
     // Spawn a task which writes large amounts of data to `input`.
     let handle = tokio::spawn(async move {
@@ -37,9 +47,46 @@ async fn main() {
     // -----------------
     // The following `await` returns quickly because the task ends with a broken
     // pipe error.
-    println!("Waiting for the task...");
+    println!("Waiting for the task...: writing({}) shutdown({})",
+             *writing.lock().unwrap(), *shutdown.lock().unwrap());
     let _ = handle.await;
 
     // However, the execution never reaches here unfortunately...
     println!("Done");
+}
+
+struct Wrapper<W> {
+    inner: W,
+    writing: Arc<Mutex<bool>>,
+    shutdown: Arc<Mutex<bool>>,
+}
+
+impl<W: AsyncWrite + Unpin> AsyncWrite for Wrapper<W> {
+    #[inline]
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context,
+        buf: &[u8]
+    ) -> Poll<io::Result<usize>> {
+        let poll = Pin::new(&mut self.inner).poll_write(cx, buf);
+        *self.writing.lock().unwrap() = poll.is_pending();
+        poll
+    }
+
+    #[inline]
+    fn poll_flush(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context
+    ) -> Poll<io::Result<()>> {
+        Pin::new(&mut self.inner).poll_flush(cx)
+    }
+
+    #[inline]
+    fn poll_shutdown(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context
+    ) -> Poll<io::Result<()>> {
+        *self.shutdown.lock().unwrap() = true;
+        Pin::new(&mut self.inner).poll_shutdown(cx)
+    }
 }
